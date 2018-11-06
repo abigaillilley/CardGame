cf81f2915ff818d511b1d5e94476ba419bd8d685
abigaillilley <abigaillilley@gmail.com>
2018-11-06 16:26:44 +0000

    abigail was here removed

--- src/cardGame.java
+++ src/cardGame.java
@@ -21,8 +21,6 @@ public class cardGame {
         System.out.println("***          Welcome to the Card Game          ***");
         System.out.println("**************************************************");
 
-        System.out.println("ABigail was here");
-
         boolean validPlayerNum = false;
         ArrayList<Integer> packHolder = new ArrayList<>();
 
